a6c699893f74956b8e9292cee09c8beb04131063
fauzaanu <hello@fauzaanu.com>
2024-11-19 00:10:50 +0500

    docs: Add docstring with project description and author information

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Author: Fauzaan Gasim
+
+OriginH is an encryption + hiding files tool.
+The name is inspired by Charles Buttowski's Father whose code name is ORION.
+
+This tool will implement an encryption that is impossible to break inspired by the bitcoin secret key design
+"""
